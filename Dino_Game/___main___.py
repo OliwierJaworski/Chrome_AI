12e57d14232da4aa9fb085cf32f404d211885e85
OLiwier_Desktop <oliwier.jaworski@student.pxl.be>
2024-12-23 16:33:53 +0100

    window is resized to playing field, move window not working in current v.

--- Dino_Game/___main___.py
+++ Dino_Game/___main___.py
@@ -5,7 +5,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from screeninfo import get_monitors
 
+monitor_used = 1
+sct_mon = monitor_used + 1
 
 import numpy as np
 import cv2
@@ -15,11 +18,12 @@ from PIL import Image
 options = Options()
 options.binary_location = "/home/oliwier-desktop/Downloads/firefox/firefox"
 options.set_preference("browser.download.folderList",2)
+options.set_preference("browser.fullscreen.autohide", True)
 options.set_preference("browser.download.manager.showWhenStarting", False)
 options.set_preference("browser.download.dir","/Data")
 options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream,application/vnd.ms-excel")
 driver = webdriver.Firefox(options=options)
-
+driver.maximize_window();
 driver.get("https://chromedino.com/")
 assert "T-Rex Dinosaur Game" in driver.title
 
@@ -42,18 +46,28 @@ runner = driver.find_element(By.CLASS_NAME, "runner-container")
 action.send_keys(" ")
 action.perform()
 
-mon = {'left': 0, 'top': 0, 'width': 1920, 'height': 1080}
-with mss as sct:
+
+monitors = get_monitors()
+
+#print(f"Monitor {monitors[monitor_used].name}: {monitors[monitor_used].width}x{monitors[monitor_used].height}")
+print(  type(monitors[monitor_used].width) )
+with mss() as sct:
+    mon1 = sct.monitors[sct_mon]
+    dis = {'left': mon1["left"]+(int)(monitors[monitor_used].width/3.275), 'top': mon1["top"]+(int)(monitors[monitor_used].height/7.2), 'width': (int)(monitors[monitor_used].width/2.56), 'height': (int)(monitors[monitor_used].height/8.2)}
     while True:
-        screenShot = sct.grab(mon)
+        screenShot = sct.grab(dis)
         img = Image.frombytes(
             'RGB', 
             (screenShot.width, screenShot.height), 
             screenShot.rgb, 
         )
+        
         cv2.imshow('test', np.array(img))
+        cv2.moveWindow("dino game inference", (int)(monitors[monitor_used].width/5),(int)(monitors[monitor_used].height/9.2))
         if cv2.waitKey(33) & 0xFF in (
             ord('q'), 
             27, 
         ):
             break
+        
+driver.close()
